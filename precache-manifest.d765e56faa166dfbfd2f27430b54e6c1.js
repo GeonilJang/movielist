78218cc3c15c9844e2e6452db031714c9031c69b
geonil <jgi92@naver.com>
2019-01-18 01:53:35 +0900

    Updates

---
+++ precache-manifest.d765e56faa166dfbfd2f27430b54e6c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bfff8fc616c83c7a549",
+    "url": "/GeonilJang/movielist.git/static/js/runtime~main.2bfff8fc.js"
+  },
+  {
+    "revision": "56d724bc8d0380b1d469",
+    "url": "/GeonilJang/movielist.git/static/js/main.56d724bc.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/GeonilJang/movielist.git/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "56d724bc8d0380b1d469",
+    "url": "/GeonilJang/movielist.git/static/css/main.65b6da2e.chunk.css"
+  },
+  {
+    "revision": "9f32c21debdacc0588b4c7712fd4b5e1",
+    "url": "/GeonilJang/movielist.git/index.html"
+  }
+];
